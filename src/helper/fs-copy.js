7c45a12ba0319cdf1091202f4ce95e01e70f9c92
Santochance <santochance@qq.com>
2017-09-08 20:09:03 +0800

    commit after  testing saveImgs() with importing ensureDir() and copy()

--- src/helper/fs-copy.js
+++ src/helper/fs-copy.js
@@ -2,36 +2,44 @@ const path = require('path')
 const fs = require('fs')
 const { random } = require('../../src/helper/randoms.js')
 
-function copy(src, dest) {
+function copy(src, dest, callback) {
   fs.readFile(src, (err, buffer) => {
     if (err) return console.error(err)
     fs.writeFile(dest, buffer, (err) => {
       if (err) {
-        console.log(err)
+        if (err) console.error (err)
         return ensureDir(dest, (err) => {
-          fs.writeFile(dest, buffer, (err) => {
-            if (err) throw err
-            console.log('try to write file again successfuly')
+          if (err) return callback(err)
 
+          fs.writeFile(dest, buffer, (err) => {
+            // if (err) throw err
+            if (err) return callback(err)
+
+            console.log('try to write file again successfuly')
+            callback()
           })
         })
       }
 
+      callback()
       console.log('write file successfuly')
     })
   })
 }
 
-
 function ensureDir(pathname, callback = () => {}) {
-  let segs = path.dirname(pathname).split(path.sep)
+  console.log('input pathname:', pathname)
+  let dir = pathname.lastIndexOf('.') > 0 ? path.dirname(pathname) : pathname
+  console.log('it will ensure dir:', dir)
+
+  let segs = dir.split(path.sep)
   console.log('segs:', segs)
 
   function next(pathname) {
-    console.log('curr is:', pathname)
+    // console.log('curr is:', pathname)
     let seg = segs.shift()
     if (seg) {
-      console.log('continue')
+      // console.log('continue')
       recur(path.join(pathname, seg))
     } else {
       console.log('finish?')
@@ -40,7 +48,7 @@ function ensureDir(pathname, callback = () => {}) {
   }
 
   function recur(pathname) {
-    console.log('curr pathname:', pathname)
+    // console.log('curr pathname:', pathname)
     fs.access(pathname, (err) => {
       if (err) {
         if (err.code === 'ENOENT') {
@@ -61,27 +69,22 @@ function ensureDir(pathname, callback = () => {}) {
   }
 
   recur(segs.shift())
-
-  console.log('finish?')
 }
-
 
 /* Test */
 const test = () => {
 
   var src = 'C:\\Users\\Vincent\\Data\\tmp\\doc.txt'
-  var dest = 'C:\\Users\\Vincent\\Data\\tmp\\a\\b\\c\\d\\e\\f\\g\\h'
+  // var dest = 'C:\\Users\\Vincent\\Data\\tmp\\a\\b\\c\\d\\e\\f\\g\\h'
 
-
-  var dynamicPath = 'C:\\Users\\Vincent\\Data\\tmp\\'
-    + Array(5).fill().map(v => (Date.now() + random(1000))
+  var dynamicPath = 'C:\\Users\\Vincent\\Data\\tmp\\' + Array(5).fill().map(v => (Date.now() + random(1000))
     .toString(16).slice(-4)).join('\\')
 
   console.log('dynamicPath:', dynamicPath)
   // ensureDir(dynamicPath)
   // ensureDir(dest)
 
-  copy(src, path.join(dynamicPath, 'deepdeepdeep.txt'))
+  copy(src, path.join(dynamicPath, 'deepdeepdeep.txt'), console.error)
 }
 
 // test()
--- src/helper/saveUploadedImages.js
+++ src/helper/saveUploadedImages.js
@@ -1,8 +1,10 @@
-const random = require('./randoms.js').random
+const { random } = require('./randoms.js')
+const { copy, ensureDir } = require('./fs-copy.js')
 
 const should = require('should')
 const path = require('path')
 const fs = require('fs')
+const util = require('util')
 
 /**
  * Helpers
@@ -11,73 +13,126 @@ function cso(obj) {
   Object.entries(obj).forEach(([key, value]) => console.log(`Inspect \`${key}\`: ${JSON.stringify(value, null, 2)}`))
 }
 
-var dir = 'C:\\Users\\Vincent\\Data\\test_uploads\\599846b6dc7fa139e04a7f44'
-var bases = Array(10).fill().map((v, i) => `${i < 10 ? '0' + i : i}.jpg`)
-var images = bases.map(base => path.join(dir, base))
-
-/* Testing */
-
-bases.should.match(/\d{2}\.jpg/)
-
-images.should.match(new RegExp(dir.replace(/\\/g, '\\\\')))
-for (let [index, img] of images.entries()) {
-  img.should.match(new RegExp(dir.replace(/\\/g, '\\\\')))
-  img.should.match(new RegExp(bases[index]))
-}
-
-var newDir = 'C:\\Users\\Vincent\\Data\\test_uploads_to\\' + Date.now().toString(16)
-
-// cso({ images })
-// cso({ newDir })
-
-function ensureDir(dir) {
+function ensureDirAsync(dir) {
   // cso({ dir })
   return new Promise((resolve, reject) => {
-    fs.access(dir, (err) => {
-      // should.exist(err)
+    ensureDir(dir, (err) => {
       if (err) {
-        fs.mkdir(dir, (err) => {
-          if (err) { /* console.error(err) */ }
-          // console.log('make new dir')
-          resolve(dir)
-        })
-      } else {
-        resolve(dir)
+        reject(err)
       }
+      resolve(dir)
     })
   })
 }
 
-function saveImgs(imgPaths, dir, imgNames = []) {
+function saveImgs(imgPaths, newDir, imgNames = []) {
+  console.log('new Dir:', newDir)
   return new Promise((resolve, reject) => {
-    ensureDir(dir)
+    ensureDirAsync(newDir)
+      .then(dir => {
+        return new Promise((resolve, reject) => {
+          fs.access(dir, (err) => {
+            if (err) reject(err)
+            resolve(dir)
+          })
+        })
+      })
+      .then(dir => {
+        console.log('has ensured dir:', dir)
+        dir.should.be.String().and.not.empty()
+        return dir
+      })
       .then(dir => {
         return Promise.all(imgPaths.map((oldPath, index) => {
           return new Promise((resolve) => {
             let basename = imgNames[index] || path.basename(oldPath)
+
+            dir.should.be.String().and.not.empty()
+            basename.should.be.String().and.not.empty()
+
             let newPath = path.join(dir, basename)
-            // fs.rename(oldPath, newPath, (err) => {
-            //   if (err) { /* 暂时忽略 */ }
-            //   console.log('file moved to newPath')
-            //   resolve(newPath)
-            // })
-            resolve(newPath)
+            // Sync placeholder
+            // resolve(newPath)
+
+            copy(oldPath, newPath, (err) => {
+              if (err) {
+                reject(err)
+              }
+              console.log('copy file \n  from  %s \n    to  %s:', oldPath, newPath)
+              // 注意这里是newPath, 因为在map()内
+              resolve(newPath)
+            })
           })
         }))
       })
+      // 注意这里是newPaths
       .then(newPaths => resolve(newPaths))
+      .catch(console.error)
   })
 }
 
-saveImgs(images, newDir)
-  .then(paths => {
+/* Test */
+const test = (testDir) => {
+  console.log('=============== Run Test =================')
 
-    // cso({ paths })
+  // 源文件夹
+  var dir = testDir || 'C:\\Users\\Vincent\\Data\\test_uploads\\599846b6dc7fa139e04a7f44'
+  // var bases = Array(4).fill().map((v, i) => `${i < 10 ? '0' + i : i}.jpg`)
 
-    for (let [index, path] of paths.entries()) {
-      path.should.match(new RegExp(newDir.replace(/\\/g, '\\\\')))
-      path.should.match(new RegExp(bases[index]))
+  // Assert: images指向的图片应该存在
+  fs.readdir(dir, (err, files) => {
+    if (err) throw err
+
+    console.log(files)
+    files.should.not.empty()
+
+    // 用于测试的pathname数组
+    // images是一组图片的pathname
+    var images = files.map(base => path.join(dir, base))
+    // 目标文件夹
+    var newDir = path.join('C:\\Users\\Vincent\\Data\\test_uploads_to\\', Date.now().toString(16), '\\')
+
+    // cso({ images })
+    cso({ dir, newDir })
+
+    /* Assertion */
+
+    // bases.should.match(/\d{2}\.jpg/)
+
+    images.should.match(new RegExp(dir.replace(/\\/g, '\\\\')))
+    for (let [index, img] of images.entries()) {
+      img.should.match(new RegExp(dir.replace(/\\/g, '\\\\')))
+      img.should.match(new RegExp(files[index]))
     }
+
+    fs.access(newDir, (err) => {
+      // Assert: 开始前目标文件夹应该不存在
+      should.exist(err)
+
+      // 把images保存到目标文件夹
+      saveImgs(images, newDir)
+        .then(pathnames => {
+
+          // cso({ pathnames })
+
+          for (let [index, pathname] of pathnames.entries()) {
+            pathname.should.match(new RegExp(newDir.replace(/\\/g, '\\\\')))
+            pathname.should.match(new RegExp(files[index]))
+          }
+
+          // Assert: 目标文件夹应该存在复制的新文件
+          fs.readdir(newDir, (err, newFiles) => {
+            should.not.exist(err)
+            newFiles.should.deepEqual(files)
+          })
+        })
+        .catch(console.error)
+    })
   })
+
+}
+
+test()
+test('C:\\Users\\Vincent\\Data\\test_uploads\\599846b6dc7fa139e04a7f45')
 
 module.exports = saveImgs
