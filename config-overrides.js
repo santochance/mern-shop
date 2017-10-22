b3816ea853b07ef032f193afc83af5f7becafbc2
Santochance <santochance@qq.com>
2017-10-22 11:39:15 +0800

    chore: 引入定制antd主题的设置

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,13 @@
+/*
+  此文件用于给react-app-rewired修改create-react-app脚手架的默认配置
+ */
 const { injectBabelPlugin } = require('react-app-rewired');
+const rewireLess = require('react-app-rewire-less');
 
 module.exports = function override(config, env) {
-  config = injectBabelPlugin(['import', { libraryName: 'antd', style: 'css' }], config);
+  config = injectBabelPlugin(['import', { libraryName: 'antd', style: true }], config);
+  config = rewireLess(config, env, {
+    modifyVars: { '@primary-color': '#1DA57A' },
+  });
   return config;
 };
