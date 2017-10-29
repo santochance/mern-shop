e43142a8bba3e4fc249de60329461e8ef2f0980b
Santochance <santochance@qq.com>
2017-10-29 15:36:23 +0800

    ProductDetails添加详情展示部分

--- src/components/ProductDetails/ProductDetails.js
+++ src/components/ProductDetails/ProductDetails.js
@@ -14,6 +14,13 @@ class ProductDetails extends React.Component {
         stocks: 130,
         sales: 40,
         commentsNum: 130,
+        detailContent: [
+          '/product_images/59f3b8913b3bf5f5c28dada2/particulars/592e35e7N0dc9ccad.jpg',
+          '/product_images/59f3b8913b3bf5f5c28dada2/particulars/592e35e8N5ebbc61e.jpg',
+          '/product_images/59f3b8913b3bf5f5c28dada2/particulars/592e35e8Ne1cd19dd.jpg',
+          '/product_images/59f3b8913b3bf5f5c28dada2/particulars/592e35e8Nee54df79.jpg',
+          '/product_images/59f3b8913b3bf5f5c28dada2/particulars/5993ae15N35429a29.jpg',
+        ], // 商品详情描述图片数组
       },
       selected: {
         amount: 1,
@@ -31,7 +38,7 @@ class ProductDetails extends React.Component {
             throw res.json()
           }
         })
-        .then(product => this.setState({ product }))
+        .then(product => this.setState({ product: Object.assign({}, this.state.product, product) }))
         .catch(console.error)
     }
   }
@@ -61,17 +68,17 @@ class ProductDetails extends React.Component {
               </span>
               <span className="old-price">
                 <span className="symbol">￥</span>
-                <span className="num">{product.oldPrice || 'xxxx'}</span>
+                <span className="num">{product.oldPrice}</span>
               </span>
             </div>
             <div className="product-data">
               <span className="comment">
                 <span className="text">累计评论</span>
-                <span className="num">{product.commentsNum || '3x'}</span>
+                <span className="num">{product.commentsNum}</span>
               </span>
               <span className="sales">
                 <span className="text">成功交易</span>
-                <span className="num">{product.sales || '2x'}</span>
+                <span className="num">{product.sales}</span>
               </span>
             </div>
             <div className="product-choice">
@@ -86,14 +93,67 @@ class ProductDetails extends React.Component {
             </div>
           </div>
         </div>
-        <div className="product-parti">
+        <div className="product-detail">
           <div>
-            这里放一个tab组件...
+            <div className="detail-header">
+              <h3 style={{
+                fontSize: 20,
+                padding: '20px 0',
+                margin: '20px 0',
+                backgroundColor: '#eee',
+              }}>商品详情</h3>
+            </div>
+            <div className="detail-content-wrap">
+              <DetailContent content={product.detailContent} />
+            </div>
           </div>
         </div>
       </div>
     )
   }
 }
+
+const DetailContent = ({ content }) => {
+  return (
+    <div className="detail-content">
+      {content.map((pic, idx) => (
+        <div className="item">
+          <img src={pic} alt="" style={{ width: '100%' }} />
+        </div>
+      ))}
+    </div>
+  )
+}
+
+// class Gallery extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {}
+//   }
+//   render() {
+//     let imgPath = '/product_images/:size/:id/:base'
+//     let picList = []
+//     return (
+//       <div className="gallery">
+//         <div className="gallery-wrap">
+//           <div className="pic-preview">
+//             <img src="" alt=""/>
+//           </div>
+//           <div className="pic-list">
+//             <a href="" className="pic-prev">>&lt;</a>
+//             <a href="" className="pic-next">&gt;</a>
+//             <ul className="pic-items">
+//               {picList.map((pic, idx) => (
+//                 <li key={idx} className="pic-item">
+//                   <img src={replacePath(imgPath, {size: 'list'})} alt=""/>
+//                 </li>
+//              ))}
+//             </ul>
+//           </div>
+//         </div>
+//       </div>
+//     )
+//   }
+// }
 
 export default ProductDetails
