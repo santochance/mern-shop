21d630a148f1a3cf75088a3a6cac989f43d2742d
Santochance <santochance@qq.com>
2017-11-13 07:25:05 +0800

    给express开启CORS

--- server/config/express.js
+++ server/config/express.js
@@ -28,6 +28,13 @@ const notice = function (req, res, next) {
 module.exports = function() {
   const app = express()
 
+  // 开启CORS
+  app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
+
   // Q: bodyParser.urlencoded() ??
   app.use(bodyParser.urlencoded({
     extended: true
